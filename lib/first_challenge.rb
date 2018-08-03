992c81b6a3a8415f9c6df8be77974c5c085e3b08
Kristina Tong <kristina.tong@gmail.com>
2018-08-03 22:50:11 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,10 @@ def first_challenge
   #your code here
   contacts.each do |person, data|
     if person == "Freddy Mercury"
-      data.each do |
+      data.each do |attribute,value|
+        if attribute == :favorite_icecream_flavors
+          value.each do |flavor|
+            
 
   #remember to return your newly altered contacts hash!
   contacts
